a036890db403b6a24535c16106df2180429f7f7e
SantiagoPachecoP <theluishop@hotmail.com>
2024-03-10 13:06:13 -0500

    Add new Branchs y exercises

--- Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
+++ Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136ad531e1a4376c4a6aab77e8bf08796fda3b46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b398dfdf239dcac2cf7ec87622c694b9d1e7f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
